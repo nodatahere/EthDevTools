a2404648947f796c2b6ccabe08b5645e3520e951
no_data_here <error.404.no.data.here@gmail.com>
2018-04-06 15:42:24 -0500

    fixed calling of cloneworkshops.sh

--- ethersetup.sh
+++ ethersetup.sh
@@ -32,10 +32,6 @@ npm install -g ganache-cli;
 npm install -g truffle;
 
 #clones workshops from http://github.com/utdcrypto to folders named by date of workshops
-wget https://raw.githubusercontent.com/nodatahere/EthDevTools/master/cloneworkshops.sh /home/$USER/cloneworkshops.sh;
-chmod +x /home/$USER/cloneworkshops.sh;
-bash /home/$USER/cloneworkshops.sh;
-rm /home/$USER/cloneworkshops.sh;
-
+curl -o- https://raw.githubusercontent.com/nodatahere/EthDevTools/master/cloneworkshops.sh | bash;
 
 exit;
